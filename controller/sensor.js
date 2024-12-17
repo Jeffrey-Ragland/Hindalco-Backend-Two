1d4908b149c67aded2402e66dc181fa719a546b3
Jeffrey-Ragland <jeffreyragland3009@gmail.com>
2024-12-17 11:42:38 +0530

    time format

--- controller/sensor.js
+++ controller/sensor.js
@@ -259,10 +259,16 @@ export const updateHindalcoProcess = async (req, res) => {
 
   if (processStatus === "Start") {
     const currentDateTime = new Date();
+    // console.log("currentdatetime", currentDateTime);
+
     const kolkataTime = currentDateTime.toLocaleString("en-US", {
       timeZone: "Asia/Kolkata",
       hour12: false,
     });
+    // console.log("kolkataTime", kolkataTime);
+
+    // const kolkatDateTime = new Date(kolkataTime);
+    // console.log("kolkataDateTime", kolkatDateTime);
 
     const calculatedStopTimeObj = new Date(
       currentDateTime.getTime() + 61 * 60 * 60 * 1000 //61 hours
@@ -534,11 +540,18 @@ export const getHindalcoProcess = async (req, res) => {
       // console.log('current time ', currentTimestamp);
       // console.log("auto stop time", stopTime);
 
+      const currentEpoch = Date.now(); // Current time in epoch
+      const stopEpoch = new Date(stopTime).getTime();
+
       if (hindalcoData && stopTime >= currentTimestamp) {
         const currentDate = parseCustomTimestamp(currentTimestamp);
         const stopDate = parseCustomTimestamp(stopTime);
 
-        const timeLeftMs = stopDate - currentDate;
+        // console.log("current epoch", currentEpoch);
+        // console.log("stop epoch", stopEpoch);
+
+        // const timeLeftMs = stopDate - currentDate;
+        const timeLeftMs = stopEpoch - currentEpoch;
 
         const timeLeft = {
           hours: Math.floor(timeLeftMs / (1000 * 60 * 60)),
@@ -552,6 +565,8 @@ export const getHindalcoProcess = async (req, res) => {
         )}h : ${String(timeLeft.minutes).padStart(2, "0")}m : ${String(
           timeLeft.seconds
         ).padStart(2, "0")}s`;
+
+        // console.log("time left string", timeLeftString);
 
         // console.log(`Time left: ${timeLeft.hours}h ${timeLeft.minutes}m ${timeLeft.seconds}s`)
         // console.log(`Time left: ${String(timeLeft.hours).padStart(2, '0')}h : ${String(timeLeft.minutes).padStart(2, '0')}m : ${String(timeLeft.seconds).padStart(2, '0')}s`)
